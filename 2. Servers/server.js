581941205855379aaa034e1b8108bbc1c8b355c4
Varsha Thirumakil <varsht612@gmail.com>
2020-08-15 07:02:53 -0400

    finish 4 chapters

---
+++ 2. Servers/server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+http.createServer(function(req, res) {
+    res.write("Hello, World!");
+    res.end();
+}).listen(8000);
+
+console.log("server started! listening on port 8080")
